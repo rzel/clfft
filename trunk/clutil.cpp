6e77711827a9a7efad86f467b01d1f1a59e97176
girish574 <girish574@c16e1510-c960-11de-a675-93be1f20d5c2>
2009-11-16 18:33:36 +0000

    intendation and modifying some APIs

--- trunk/clutil.cpp
+++ trunk/clutil.cpp
@@ -9,212 +9,217 @@ cl_command_queue commandQueue;
 
 
 
-cl_int init_cl_context(cl_device_type device_type)
+cl_int 
+init_cl_context(const cl_device_type device_type)
 {
-  cl_int ciErrNum = CL_SUCCESS; 
+    cl_int ciErrNum = CL_SUCCESS; 
 
-  cxContext = clCreateContextFromType(0, /* cl_context_properties */
-				      device_type,
-				      NULL, /* error function ptr */
-				      NULL, /* user data to be passed to err fn */
-				      &ciErrNum);
+    cxContext = clCreateContextFromType(0, /* cl_context_properties */
+  				        device_type,
+				        NULL, /* error function ptr */
+				        NULL, /* user data to be passed to err fn */
+				        &ciErrNum);
 
-  fprintf(stdout,"After createContext ..\n");
+    fprintf(stdout,"After createContext ..\n");
 
-  if(ciErrNum != CL_SUCCESS)
-    {
-      fprintf(stderr,"Error: Context Creation Failed:%d!\n", ciErrNum);
+    if(ciErrNum != CL_SUCCESS) {
+        fprintf(stderr,"Error: Context Creation Failed:%d!\n", ciErrNum);
     }
 
-  return ciErrNum;
+    return ciErrNum;
 }
 
-cl_int getDeviceCount(cl_uint * ciDeviceCount)
+cl_int 
+getDeviceCount(cl_uint& ciDeviceCount)
 {
-  cl_int ciErrNum = CL_SUCCESS;
-  size_t nDeviceBytes;
+    size_t nDeviceBytes;
   
-  ciErrNum = clGetContextInfo(cxContext,
-			      CL_CONTEXT_DEVICES, /* Param Name */
+    const cl_int ciErrNum = clGetContextInfo(cxContext,
+	          	      CL_CONTEXT_DEVICES, /* Param Name */
 			      0, /* size_t param_value_size  */
 			      NULL, /* void * param_value */
 			      &nDeviceBytes); /* size_t param_value_size_ret */
   
-  if(ciErrNum != CL_SUCCESS)
-    {
-      fprintf(stderr,"Error in GetDeviceInfo!\n");
-      return ciErrNum;
+    if (ciErrNum != CL_SUCCESS) {
+        fprintf(stderr,"Error in GetDeviceInfo!\n");
+        return ciErrNum;
     }
 
-  *ciDeviceCount = (cl_uint)nDeviceBytes/sizeof(cl_device_id);
+    ciDeviceCount = (cl_uint)nDeviceBytes/sizeof(cl_device_id);
 
-  if(*ciDeviceCount == 0)
-    fprintf(stderr, "No Devices Supporting OpenCL!!\n");
+    if (ciDeviceCount == 0) {
+        fprintf(stderr, "No Devices Supporting OpenCL!!\n");
+    }
 
-  return ciErrNum;
+    return ciErrNum;
 }
       
 
-cl_int createCommandQueue(cl_device_id * device , int device_id)
+cl_int 
+createCommandQueue(const unsigned deviceId)
 {
-  cl_int ciErrNum = CL_SUCCESS;
+    const cl_device_id device = oclGetDev(cxContext, deviceId);
+    cl_int ciErrNum = CL_SUCCESS;
+    commandQueue = clCreateCommandQueue(cxContext, 
+				        device,
+				        0,
+				        &ciErrNum);
 
-  *device = oclGetDev(cxContext, device_id);
-  commandQueue = clCreateCommandQueue( cxContext, 
-				       *device,
-				       0,
-				       &ciErrNum);
-
-  if(ciErrNum != CL_SUCCESS)
-    {
-      fprintf(stderr, "Could not create CommandQueue !!\n");
+    if (ciErrNum != CL_SUCCESS) {
+        fprintf(stderr, "Could not create CommandQueue !!\n");
     }
 
-  return ciErrNum;
+    return ciErrNum;
 }				      
 
-cl_int compile_program(char * argv[] , char * header_file, 
-		       char * kernel_file,
-		       cl_program * cpProgram) /* Program object stored in here. */
+cl_int 
+compileProgram(const char* const argv[] , const char* const header_file, 
+               const char* const kernel_file,
+               cl_program& cpProgram) /* Program object stored in here. */
 {
-  cl_int ciErrNum;
-  size_t program_length;
-  const char * header_path = shrFindFilePath(header_file, argv[0]);
+    const char* header_path = shrFindFilePath(header_file, argv[0]);
   
-  char * header = oclLoadProgSource( header_path, "" , &program_length);
-  char * source_path, * source;
+    size_t program_length;
+    char* header = oclLoadProgSource(header_path, "" , &program_length);
 
-  if(header == NULL)
-    {
-      fprintf(stderr,"Error: Failed to load the header %s!\n", header_path);
-      return -1000;
+    if(header == NULL) {
+        fprintf(stderr,"Error: Failed to load the header %s!\n", header_path);
+        return -1000;
     }
 
-  source_path = shrFindFilePath(kernel_file, argv[0]);
-  source = oclLoadProgSource(source_path, header, &program_length);
+    const char* source_path = shrFindFilePath(kernel_file, argv[0]);
+    char* source = oclLoadProgSource(source_path, header, &program_length);
 
-  if(!source)
-    {
-      fprintf(stderr, "Error: Failed to load source %s !\n", source);
-      return -2000;
+    if (!source) {
+        fprintf(stderr, "Error: Failed to load source %s !\n", source);
+        return -2000;
     }
 
-  *cpProgram = clCreateProgramWithSource(cxContext, 
-					1,
-					(const char **) &source,
-					&program_length,
-					&ciErrNum);
-
-  free(header);
-  free(source);
-  if(ciErrNum != CL_SUCCESS)
-    {
-      fprintf(stderr, "Error: Failed to create Program!\n");
-      return ciErrNum;
+    cl_int ciErrNum;
+    cpProgram = clCreateProgramWithSource(cxContext, 1,
+					   (const char **) &source,
+					   &program_length,
+					   &ciErrNum);
+    // do we need these frees?
+    free(header);
+    free(source);
+    if (ciErrNum != CL_SUCCESS) {
+        fprintf(stderr, "Error: Failed to create Program!\n");
+        return ciErrNum;
     }
       
-  /* Build program */
+    /* Build program */
 
-  ciErrNum = clBuildProgram(*cpProgram,
-			    0, 	/* Number of devices for which we need to do this */
-			    NULL, /* Device List */
-			    "-cl-mad-enable",
-			    NULL, /* ptr to function */
-			    NULL); /* User data to pass to ptrfn */
+    ciErrNum = clBuildProgram(cpProgram,
+        		      0, 	/* Number of devices for which we need to do this */
+			      NULL, /* Device List */
+			      "-cl-mad-enable",
+			      NULL, /* ptr to function */
+			      NULL); /* User data to pass to ptrfn */
 
-  if(ciErrNum != CL_SUCCESS)
-    {
-      fprintf(stderr,"Error: Compilation Failure! \n");
+    if (ciErrNum != CL_SUCCESS) {
+        fprintf(stderr,"Error: Compilation Failure! \n");
     }
 
-  return ciErrNum;
+    return ciErrNum;
 }
 
-cl_int  createKernel(cl_program cpProgram, 
-		     char * kernelName,
-		     cl_kernel *kernobj)
+cl_int  
+createKernel(const cl_program& cpProgram, 
+	     const char* const kernelName,
+	     cl_kernel& kernobj)
 {
 
-  cl_int ciErrNum = CL_SUCCESS;
-
-  *kernobj = clCreateKernel(cpProgram, kernelName, &ciErrNum);
-
-  if(ciErrNum != CL_SUCCESS)
-    fprintf(stderr, "Error: Failed to Create Kernel: %s!\n", kernelName);
-      
-  return ciErrNum;
+    cl_int ciErrNum = CL_SUCCESS;
+    kernobj = clCreateKernel(cpProgram, kernelName, &ciErrNum);
+    if (ciErrNum != CL_SUCCESS) {
+        fprintf(stderr, "Error: Failed to Create Kernel: %s!\n", kernelName);
+    }
+    return ciErrNum;
 }
 
 
-cl_mem createDeviceBuffer(cl_mem_flags flags, size_t size,void * host_ptr,
-			  int copytoDevice)
+cl_mem 
+createDeviceBuffer(const cl_mem_flags flags, 
+                   const size_t size, void* const hostPtr,
+	           const bool copyToDevice)
 {
-  cl_int ciErrNum = CL_SUCCESS;
-  cl_mem d_mem;
-  d_mem = clCreateBuffer(cxContext,
-		 flags,
-		 size,
-		 host_ptr,
-		 &ciErrNum);
+    cl_int ciErrNum = CL_SUCCESS;
+    const cl_mem d_mem = clCreateBuffer(cxContext,
+ 	 	                        flags,
+		                        size,
+		                        hostPtr,
+		                        &ciErrNum);
   
-  if(ciErrNum != CL_SUCCESS)
-    {
-      fprintf(stderr,"Couldnt create device buffer !!");
-      return NULL;
+    if (ciErrNum != CL_SUCCESS) {
+        fprintf(stderr,"Couldnt create device buffer !!");
+        return NULL;
     }
 
-  if(copytoDevice)
-    {
+    if (copyToDevice) {
       fprintf(stderr,"Copyting to device memory.\n");
       clEnqueueWriteBuffer(commandQueue, d_mem, CL_TRUE, 0, size,
-			   host_ptr, 0, NULL, NULL);
+			   hostPtr, 0, NULL, NULL);
     }
 
-  return d_mem;
+    return d_mem;
 }
 
 
 
-cl_int runKernel(cl_kernel kernobj, cl_uint workDim, size_t localWorkSize[],
-		 size_t  globalWorkSize[])
+cl_int 
+runKernel(const cl_kernel kernobj, const cl_uint workDim, 
+          const size_t localWorkSize[], const size_t globalWorkSize[])
 {
 
-  cl_event GPUExecution;
-  cl_int ciErrNum;
+    cl_event GPUExecution;
 
-  fprintf(stderr,"Before Kernel .. kernobj:%x globalWorkSize:%x localWorkSize:%x \n", kernobj, *globalWorkSize, *localWorkSize);
-  ciErrNum = clEnqueueNDRangeKernel(commandQueue, kernobj, workDim, NULL, 
-			 globalWorkSize, localWorkSize, 0, NULL,
-			 NULL);
-  fprintf(stderr,"After kernel..\n");
+    const cl_int ciErrNum = clEnqueueNDRangeKernel(commandQueue, kernobj, 
+                                                   workDim, NULL, 
+                                	           globalWorkSize, 
+                                                   localWorkSize, 0, 
+                                                   NULL, NULL);
+    fprintf(stderr,"After kernel..\n");
 
-  if(ciErrNum != CL_SUCCESS)
-    {
+    if (ciErrNum != CL_SUCCESS) {
 
-      fprintf(stderr,"Kernel Enqueue not a success !!\n");
+        fprintf(stderr,"Kernel Enqueue not a success !!\n");
 
-      switch(ciErrNum)
-	{
-	case CL_INVALID_WORK_GROUP_SIZE: fprintf(stderr,"CL_INVALID_WORK_GROUP_SIZE\n"); break;
-	case CL_INVALID_WORK_ITEM_SIZE: fprintf(stderr,"CL_INVALID_WORK_GROUP_SIZE\n"); break;
-	case CL_INVALID_WORK_DIMENSION: fprintf(stderr,"CL_INVALID_WORK_DIMENSION\n"); break;  
-	case CL_INVALID_KERNEL: fprintf(stderr,"CL_INVALID_KERNEL\n"); break;
-	case CL_INVALID_KERNEL_ARGS: fprintf(stderr,"CL_INVALID_KERNEL_ARGS\n"); break;
-	case CL_INVALID_VALUE : fprintf(stderr,"CL_INVALID_VALUE\n"); break;
-	default : fprintf(stderr,"Unknown case %d \n",ciErrNum);
-	}
+        switch(ciErrNum) {
+            case CL_INVALID_WORK_GROUP_SIZE: 
+            fprintf(stderr,"CL_INVALID_WORK_GROUP_SIZE\n"); 
+            break;
+	    case CL_INVALID_WORK_ITEM_SIZE: 
+            fprintf(stderr,"CL_INVALID_WORK_GROUP_SIZE\n"); 
+            break;
+	    case CL_INVALID_WORK_DIMENSION: 
+            fprintf(stderr,"CL_INVALID_WORK_DIMENSION\n"); 
+            break;  
+	    case CL_INVALID_KERNEL: 
+            fprintf(stderr,"CL_INVALID_KERNEL\n"); 
+            break;
+	    case CL_INVALID_KERNEL_ARGS: 
+            fprintf(stderr,"CL_INVALID_KERNEL_ARGS\n"); 
+            break;
+	    case CL_INVALID_VALUE: 
+            fprintf(stderr,"CL_INVALID_VALUE\n"); 
+            break;
+	    default: 
+            fprintf(stderr,"Unknown case %d \n",ciErrNum);
+        }
       
     }
   
-  return ciErrNum;
+    return ciErrNum;
 }
 
-void copyfromDevice(cl_mem d_Mem, size_t size, void * h_Mem, cl_int deviceCount)
+void 
+copyFromDevice(const cl_mem dMem, const size_t size, 
+               void* const hMem, const cl_int deviceCount)
 {
-  cl_event GPUDone;
-  clEnqueueReadBuffer(commandQueue, d_Mem,CL_TRUE, 0, size,
-		      h_Mem, 0, NULL, &GPUDone);
+    cl_event GPUDone;
+    clEnqueueReadBuffer(commandQueue, dMem,CL_TRUE, 0, size,
+  		        hMem, 0, NULL, &GPUDone);
 
-  clWaitForEvents(deviceCount, &GPUDone);
-  
+    clWaitForEvents(deviceCount, &GPUDone);
 }
--- trunk/clutil.h
+++ trunk/clutil.h
@@ -9,26 +9,27 @@
 
 cl_int init_cl_context(cl_device_type device_type);
 
-cl_int getDeviceCount(cl_uint * ciDeviceCount);
+cl_int getDeviceCount(cl_uint& ciDeviceCount);
 
-cl_int createCommandQueue(cl_device_id * device, int device_id);
+cl_int createCommandQueue(const unsigned deviceId);
 
-cl_int compile_program(char * argv[] , char * header_file, 
-		       char * kernel_file,
-		       cl_program * cpProgram); /* Program object stored in here. */
+cl_int compileProgram(const char* const argv[] , const char* const header_file, 
+		       const char* const kernel_file,
+		       cl_program& cpProgram); /* Program object stored in here. */
 
 
-cl_int  createKernel(cl_program cpProgram, 
-		     char * kernelName,
-		     cl_kernel *kernobj);
+cl_int  createKernel(const cl_program& cpProgram, 
+		     const char* const kernelName,
+		     cl_kernel& kernobj);
 
-cl_mem createDeviceBuffer(cl_mem_flags flags, size_t size,void * host_ptr,
-			  int copytoDevice);
+cl_mem createDeviceBuffer(const cl_mem_flags flags, const size_t size,
+                          void* const  hostPtr, const bool copytoDevice);
 
-cl_int runKernel(cl_kernel  kernobj, cl_uint workDim, size_t * localWorkSize,
-		 size_t * globalWorkSize);
+cl_int runKernel(const cl_kernel  kernobj, const cl_uint workDim, 
+                 const size_t* localWorkSize, const  size_t* globalWorkSize);
 
-void copyfromDevice(cl_mem d_Mem, size_t size, void * h_Mem, cl_int deviceCount);
+void copyFromDevice(const cl_mem dMem, const size_t size, 
+                    void* hMem, const cl_int deviceCount);
 
 extern cl_command_queue commandQueue;
 extern cl_context cxContext ;
