d65a0992b8614363e386345c7b63e7dd9af0b8eb
girish574 <girish574@c16e1510-c960-11de-a675-93be1f20d5c2>
2009-12-07 03:03:03 +0000

    fftw

--- fftw/fftw.cpp
+++ fftw/fftw.cpp
@@ -7,30 +7,34 @@
 #include <sys/wait.h>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
  {
-         const unsigned N = 1048576;
+         if (argc == 1) {
+             cout << "./a.out N" << endl;
+             return 0;
+         } 
+         const unsigned N = atoi(argv[1]);
+         const unsigned size = 1048576;
          fftw_complex *in, *out;
          fftw_plan p;
          pid_t pid;
-         if ((pid = fork())== 0) {
-         in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-         out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-         p = fftw_plan_dft_1d(65536, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+         in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+         out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+          struct rusage start;
+         getrusage(RUSAGE_SELF, &start);
+
+         p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
          fftw_execute(p); /* repeat as needed */
          fftw_destroy_plan(p);
           fftw_free(in); fftw_free(out);
-      }
-      int status;
-      wait(&status);
 
 
           struct rusage end;
-         getrusage(RUSAGE_CHILDREN, &end);
-         cout << end.ru_stime.tv_sec
+         getrusage(RUSAGE_SELF, &end);
+         cout << end.ru_stime.tv_sec - start.ru_stime.tv_sec
               <<  "seconds " <<  endl;
 
-         cout << end.ru_stime.tv_usec
+         cout << end.ru_stime.tv_usec - start.ru_stime.tv_usec
               <<  "micro seconds " <<  endl;
 
 }
